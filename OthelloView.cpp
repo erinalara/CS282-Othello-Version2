e3db75ad82ed2fdeb9cc5d667cf83cacb54aac18
erinalara <48671321+erinalara@users.noreply.github.com>
2020-05-01 05:51:20 -0700

    Update OthelloView.cpp

--- OthelloView.cpp
+++ OthelloView.cpp
@@ -24,10 +24,6 @@ void OthelloView::PrintBoard(std::ostream& s) const {
     }
 }
 
-// Creates and returns ownership of an OthelloMove object by parsing a string
-// of the format (r, c) and constructing a corresponding Move object. Yields
-// ownership of the created Move.
-// You can assume that the string contains a valid move format.
 
 unique_ptr<OthelloMove> OthelloView::ParseMove(const std::string& strFormat) {
     
@@ -37,34 +33,19 @@ unique_ptr<OthelloMove> OthelloView::ParseMove(const std::string& strFormat) {
     parse >> p1 >> row >> comma >> col >> p2;
     unique_ptr<OthelloMove> move = make_unique<OthelloMove>(BoardPosition(row, col));
     return move;
-    /*
-    int row = strFormat[1];
-    int col = strFormat[4];
-    unique_ptr<OthelloMove> move = make_unique<OthelloMove>(BoardPosition(row, col));
-    //mOthelloBoard->mBoard[row][col] = move((int)temp);
-    //return temp;
-    //return make_unique<OthelloMove>(BoardPosition(row, col));
-    //return make_unique<OthelloMove> (mOthelloBoard->mBoard[row][col]);
-    return move;
-    */
 
 
 }
 
 
-// Outputting a View object causes it to print its owned Board to the output stream.
-// The printed output should include the board itself, and the current player.
-// LINE LIMIT: 3 lines.
+
 ostream& operator<< (std::ostream& lhs, const OthelloView& rhs) {
     rhs.PrintBoard(lhs);
     int m = (int) rhs.mOthelloBoard->GetCurrentPlayer();
-    return lhs << m;
+    return lhs << "Player " << m << "'s turn.";
 }
 
-// It doesn't belong to the OthelloView class, but we still consider this operator
-// to be part of the "view". This operator prints a Move by printing its position.
-// Reminder: OthelloMove has operator std::string.
-// LINE LIMIT: 1 line.
+
 ostream& operator<<(std::ostream& lhs, const OthelloMove& rhs) {
     return lhs << string(rhs);
 }
